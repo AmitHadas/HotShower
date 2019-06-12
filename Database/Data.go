6696501c9be8c1a9acf7080470828154ba26b33d
Amit Cohen <amitc@mellanox.com>
2019-06-12 12:35:48 +0300

    Add initial files

---
+++ Database/Data.go
@@ -0,0 +1,47 @@
+package Database
+
+type Boiler struct {
+	ID 				int
+	Desired_Temp	int
+	Max_Temp    	int
+}
+
+type User struct {
+	User_Name 	string
+	Pass    	string
+	Boiler_ID   int
+}
+
+var UserMap = make(map[string]User)
+var BoilerMap = make(map[int]Boiler)
+
+func AddNewBoiler(boiler_id int, desired_temp int, max_temp int){
+	new_boiler := new(Boiler{boiler_id, desired_temp, max_temp})
+	BoilerMap[boiler_id] = new_boiler
+}
+
+func AddNewUser(user User, desired_temp int, max_temp int) {
+	UserMap[user.User_Name] = user
+	boiler, ok := BoilerMap[user.Boiler_ID]
+	if !ok {
+		AddNewBoiler(user.Boiler_ID, desired_temp, max_temp)‏
+	} else {
+		boiler.Desired_Temp = desired_temp
+		boiler.Max_Temp = max_temp
+	}
+}
+
+func GetBoilerByUserName(userName string, pass string) *Boiler {
+
+	user, valid_user := UserMap[userName]
+	if valid_user {
+		if user.Pass == pass {
+			boiler, valid_boiler := BoilerMap[user.Boiler_ID]
+
+			if valid_boiler {
+				return &boiler
+			}
+		}
+	}
+	return nil
+}
